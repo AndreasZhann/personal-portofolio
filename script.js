6df5ee08d25f4626eb1c27bfeae1e1b8ee1d50f8
AndreasZhann <andreaszhan4@gmail.com>
2025-04-10 10:39:29 +0700

    fix save data bug

--- script.js
+++ script.js
@@ -1,34 +1,33 @@
 const loadMoreBtn = document.querySelector("#loadMore");
-let currentItem = parseInt(localStorage.getItem("sh"));
-let boxes = [...document.querySelectorAll(".projectContainer .boxContainer .box")];
+const boxes = [...document.querySelectorAll(".projectContainer .boxContainer .box")];
+let currentItem = parseInt(localStorage.getItem("shownItems")) || 3;
 
 const showItems = (count) => {
   boxes.forEach((box, index) => {
     box.style.display = index < count ? "inline-block" : "none";
   });
+
   loadMoreBtn.innerText = count >= boxes.length ? "Show Less" : "Load More";
 };
+
 showItems(currentItem);
 
 loadMoreBtn.onclick = () => {
   if (currentItem >= boxes.length) {
+    // Kalau semua project sudah muncul dan tombol Show Less diklik
     currentItem = 3;
   } else {
+    // Tambah 3 project
     currentItem += 3;
-    if (currentItem > boxes.length) {
-      currentItem = boxes.length;
-    }
+    if (currentItem > boxes.length) currentItem = boxes.length;
   }
-  localStorage.setItem("showItems", currentItem);
+
+  localStorage.setItem("shownItems", currentItem);
   showItems(currentItem);
 };
+
+// Jaga-jaga kalau kembali dari TryOn, tetap tunjukkan jumlah project terakhir
 window.addEventListener("pageshow", function (event) {
-  currentItem = 3;
+  currentItem = parseInt(localStorage.getItem("shownItems")) || 3;
   showItems(currentItem);
 });
-const faders = document.querySelectorAll(".fade-in");
-
-const appearOptions = {
-  threshold: 0.1,
-  rootMargin: "0px 0px -50px 0px",
-};
